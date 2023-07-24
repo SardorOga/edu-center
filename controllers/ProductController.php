a6226757095bf4d3f9007f3fa0bc47fa13bce275
webcoderphp <asadbekdev007@gmail.com>
2023-07-24 19:53:01 +0500

    add new functions in PostController.php

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -13,6 +13,62 @@ use vendor\myframe\Views;
 
 class ProductController extends Controller
 {
+
+    function getCoursesBySearch($column,$search,$page)
+    {
+        include "./config/config.php";
+        $offset = ($page - 1) * PAGE_LIMIT;
+        $search = '%'.$search.'%';
+        $sql = "select * from courses c 
+                inner join teachers t on t.teacher_id = c.teacher_id 
+                where ".$column." like '".$search."' limit ".$offset.",".PAGE_LIMIT;
+        /** @var PDO $con */
+        $stmt = $con->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+//SELECT * FROM  (SELECT * FROM students LIMIT 0,5) AS students WHERE name LIKE `1` OR name LIKE `%1%`
+
+
+    /** Course table */
+
+    function getList(float|int|string $id)
+    {
+        include "./config/config.php";
+        $sql = 'SELECT * FROM courses WHERE course_id = :id';
+        /** @var PDO $con */
+        $stmt = $con->prepare($sql);
+        $stmt->execute([':id'=>$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+//function getCourses()
+//{
+//    include "./config/config.php";
+//    $sql = 'select * from courses c inner join teachers t on t.teacher_id = c.teacher_id';
+//    /** @var PDO $con */
+//    $stmt = $con->prepare($sql);
+//    $stmt->execute();
+//    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+//}
+    /** get data from courses table with limit or without */
+    function getCourses($page = 1,$withoutLimit =  false)
+    {
+        $sql = 'select * from courses c inner join teachers t on t.teacher_id = c.teacher_id';
+        if(!$withoutLimit){
+            $sql = 'select * from courses c inner join teachers t on t.teacher_id = c.teacher_id  LIMIT :offset, :limit';
+        }
+        return retrieveAllData($page,$sql);
+    }
+
+    /** get row's count of course */
+    function getCoursePageCount()
+    {
+        $sql = 'select * from courses';
+        return getPageCount($sql);
+    }
     public function create()
     {
         session_start();
